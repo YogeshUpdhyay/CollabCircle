ffe13da426e82cf8f8c0527217c84c27495d925e
dhirajssh <60935022+dhirajssh@users.noreply.github.com>
2021-01-12 17:59:43 +0530

    browse Projects

--- src/Pages/BrowseProject/BrowseProject.js
+++ src/Pages/BrowseProject/BrowseProject.js
@@ -2,13 +2,22 @@ import React from 'react'
 import Navbar from '../../components/Layout/NavBar/NavBar'
 import ProjectCard from './components/ProjectCard'
 import browse from '../../assets/BrowseProject.png'
+import "./BrowseProject.css"
 
 function BrowseProjects() {
+
+  const projects = [{name:'Mast',description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique enim voluptatem rem tempora culpa ipsa iusto placeat quaerat, minima praesentium necessitatibus aliquid facilis dolorem, nulla quis ullam eius dolore dolores.', vacancies:'3',date:'yay'},{name:'Trump',description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique enim voluptatem rem tempora culpa ipsa iusto placeat quaerat, minima praesentium necessitatibus aliquid facilis dolorem, nulla quis ullam eius dolore dolores.', vacancies:'4',date:'yayyy'}, {name:'Trump',description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique enim voluptatem rem tempora culpa ipsa iusto placeat quaerat, minima praesentium necessitatibus aliquid facilis dolorem, nulla quis ullam eius dolore dolores.', vacancies:'4',date:'yayyy'}]
+
   return (
-    <div>
+    <div className="pb-4">
       <Navbar />
       {/* <img src={browse} style={{width: "100vw",height: 'auto'}}/> */}
-      <h1>Browse Projects</h1>
+      <div className="container">
+        <h2 className="intro-header">Browse Projects</h2>
+        {projects.map(project=>(
+          <ProjectCard project={project}/>
+        ))}
+      </div>
     </div>
   )
 }
--- src/Pages/BrowseProject/components/ProjectCard.js
+++ src/Pages/BrowseProject/components/ProjectCard.js
@@ -1,12 +1,32 @@
 import { Card } from '@material-ui/core'
 import React from 'react'
+import './ProjectCard.css'
 
-function ProjectCard() {
+function ProjectCard({project}) {
   return (
     <div>
-      <Card>
-        Hello World
-      </Card>
+      
+        <div className="card py-3 px-2 mb-5 pcard" style={{width:'100%', backgroundColor: '#8236E4',color:'#FEF6FF'}}>
+          <div className="card-title"><h4>{project.name}</h4></div>
+          <div className="card-body" style={{transform: 'translateY(-13%)'}}>
+          {project.description}
+          
+          </div>
+          <div className="row">
+            <div className="col-6">
+              Vacancies : {project.vacancies}
+            </div>
+            <div className="col-6">
+              Date:{project.date}
+            </div>
+            
+          </div>
+
+          <div className="mt-4" style={{width:'100%',display:'flex',justifyContent: 'center', alignItems: 'center'}}>
+              <input type="submit" className="btn" value="View Project" style={{fontWeight:'600'}}/>
+            </div>
+
+        </div>
     </div>
   )
 }
