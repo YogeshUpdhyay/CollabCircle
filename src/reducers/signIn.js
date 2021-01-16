3d63e131dbef139ceef11c932d8a80f700dbf805
dhirajssh <60935022+dhirajssh@users.noreply.github.com>
2021-01-16 14:28:06 +0530

    added my branch

--- src/reducers/signIn.js
+++ src/reducers/signIn.js
@@ -3,7 +3,7 @@ const signInReducer = (state={data: {}, isLogged: false}, action)=>{
     case "SIGNIN":
       console.log(action.payload.access_token);
       if(action.payload.access_token){
-        state.isLogged = true;
+        state.isLogged = true;// so i have tried creating a global state for logged in but for some reason it is still acting like local state, like the value is changing from component to component
       }
       return {...state, data: action.payload};
     default:
