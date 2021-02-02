6e5241fb9ff6a210904df917280b82c8b719b712
Divya <dsrao0712@gmail.com>
2021-02-02 20:09:59 +0530

    Create projects with react hook form

--- src/Pages/Projects/CreateProjects/CreateProjects.js
+++ src/Pages/Projects/CreateProjects/CreateProjects.js
@@ -1,4 +1,4 @@
-import React ,{ useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 //components
 import NavBar from "../../../components/Layout/NavBar/NavBar";
 import CreateProject from "../../../assets/createProject.png";
@@ -13,21 +13,17 @@ import {
 } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import { makeStyles } from "@material-ui/core/styles";
-import { Formik, Form, Field , useFormik } from 'formik';
-import MuiSelect, {
-  SelectProps as MuiSelectProps,
-} from '@material-ui/core/Select';
-import * as Yup from 'yup';
 import theme from "../../../components/Theme/theme";
 import { ThemeProvider } from "@material-ui/core/styles";
-
+//Form
+import {useForm , Controller} from 'react-hook-form' ;
 //css
 
 const useStyles = makeStyles({
   card: {
     maxWidth: "600px",
-    margin : "50px" ,
-    padding : '10px'
+    margin: "50px",
+    padding: '10px'
   },
   container: {
     display: "Flex",
@@ -37,68 +33,52 @@ const useStyles = makeStyles({
     display: "flex",
     justifyContent: "flex-start",
     alignItems: "center",
- 
+
   },
   btn_submit: {
     marginTop: "2em",
   },
-  img_createProject : {
-    maxWidth : '400px'
+  img_createProject: {
+    maxWidth: '400px'
+  },
+  errorMessage : {
+    color :"red",
+    display : "flex" ,
+    justifyContent: "flex-start"
   }
 
 });
 
-const ValidationSchema = Yup.object({
-
-  title : Yup.string().required("Required"),
-  description : Yup.string().required("Required"),
-  skills_req : Yup.string().required("Required"),
-  vacancy : Yup.string().required("Required")
-
-})
 
 export default function CreateProjects() {
-  const [skill , setSkill]  = useState([]);
-  useEffect(() =>{
-    console.log(skill) 
-  } , [skill])
- 
+
   const classes = useStyles();
-  const {values , handleSubmit , handleChange , errors } = useFormik({
-    initialValues : {
-      title : "" , 
-      description : "" ,
-      skills_req : {
-        title : "",
-        key: ""
-      },
-      vacancy : "" 
-    },
+  const {register , handleSubmit , errors , control , setValue} = useForm();
+  const onSubmit = (data) => {
+    console.log(data) ;
+  }
 
-    ValidationSchema ,
-
-    onSubmit(values){
-      console.log(values)
-    }
-
-  });   
-
+  const [tags, setTags] = useState({})
+  console.log(tags)
+ console.log(errors);
   return (
     <ThemeProvider theme={theme}>
       <div>
         <NavBar />
 
         <Grid container>
-          <Grid item lg={6} md={12} xs={12} sm = {12}>
+          <Grid item lg={6} md={12} xs={12} sm={12}>
             <Grid container>
-              <Grid item xs={12} md={12 } lg={12}>
+              <Grid item xs={12} md={12} lg={12}>
                 <img src={CreateProject} style={{ maxWidth: "400px" }}></img>
               </Grid>
 
               <Grid item lg={12} md={12} xs={12}>
+
                 <Typography variant="h5" style={{ color: "white" }}>
                   Got an idea for project ?
                 </Typography>
+
                 <Typography variant="h6" style={{ color: "white" }}>
                   Post your project idea and find a partner to collab
                 </Typography>
@@ -115,7 +95,7 @@ export default function CreateProjects() {
           >
             <Grid container variant="outlined" spacing={2}>
               <Grid item lg={12} xs={12} sm={12}>
-                <form onSubmit={handleSubmit}>
+                <form  onSubmit={handleSubmit(onSubmit)}>
                   <Card className={classes.card}>
                     <CardContent>
                       <h4>Create Project</h4>
@@ -130,12 +110,11 @@ export default function CreateProjects() {
                             name = "title"
                             lg={12}
                             fullWidth="true"
-                            inputProps={{ maxLength: 50 }}
-                            onChange={handleChange}
-                            values={values.title}
+                            defaultValue = ""
+                            inputRef= {register({required: true})}
                           >
                           </TextField>
-                          {errors.title? errors.title: null}
+                          {errors.title && <span className={classes.errorMessage}>This field is required</span>}
                         </Grid>
                         <Grid item lg={12} md={12} xs={12}>
                           <TextField
@@ -144,41 +123,42 @@ export default function CreateProjects() {
                             label="Description"
                             lg={12}
                             fullWidth="true"
-                            name = "description"
-                            inputProps={{ maxLength: 400 }}
-                            onChange={handleChange}
-                            values = {values.description}
+                            name="description"
+                            inputRef = {register({required: true})}
                           >
-                        
                           </TextField>
-                          {errors.description ? errors.description: null}
+                          {errors.description && <span className={classes.errorMessage}>This field is required</span>}
                         </Grid>
                         <br></br>
                         <Grid item lg={12} md={12} xs={12}>
-                          <Autocomplete
-                            multiple
-                            id="tags-standard"
-                            options={skills}
-                            getOptionLabel={(option) => option.title}
+                          <Controller
+                            name = "skills_req"
+                            control = {control}
                             
-                            values = {values.skills_req}
-
-                            onChange={(e ,values) => {
-                              setSkill(values);
-                            }}
-                            renderInput={(params) => (
-                              <TextField
-                                {...params}
-                                variant="standard"
-                                name = "skills_req"
-                                label="Skills required"
-                                placeholder="Enter the skills required"
-  
-                              >
-                              </TextField>
-                              
+                            render={(
+                              { onChange, onBlur, value, name, ref },
+                              { invalid, isTouched, isDirty }
+                            ) => (
+                              <Autocomplete
+                              multiple
+                              options={skills}
+                              getOptionLabel={(option) => option}
+                              defaultValue={[skills[3]]}
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  variant="standard"
+                                  label="Skills Required"
+                                  placeholder="Skills required"
+                                  inputRef = {ref}
+                                  onChange =  {(e) => {setTags(e.target.value)}}
+                                  value = {tags}
+                                />
+                              )}
+                            />
                             )}
-                          /> 
+                          >
+                          </Controller>
                         </Grid>
 
                         <Grid item lg={12} md={12} xs={12}>
@@ -192,13 +172,13 @@ export default function CreateProjects() {
                             }}
                             fullWidth="true"
                           >
-                            
+
                           </TextField>
                         </Grid>
 
                         <Grid container justify="flex-end">
                           <Button
-                            type = "submit"
+                            type="submit"
                             variant="contained"
                             color="secondary"
                             className={classes.btn_submit}
@@ -221,29 +201,36 @@ export default function CreateProjects() {
 
 //SKills dropdown options
 
+// const skills = [ 
+//   { title: "Java", key: "1" },
+//   { title: "JavaScript", key: "2" },
+//   { title: "React.js", key: "3" },
+//   { title: "Angular.js", key: "4" },
+//   { title: "Vue.js", key: "5" },
+//   { title: "Python", key: "6" },
+//   { title: "Go", key: "7" },
+//   { title: "Node.js", key: "8" },
+//   { title: "PHP", key: "9" },
+//   { title: "JQuery", key: "10" },
+//   { title: "Git", key: "11" },
+//   { title: "SQL", key: "12" },
+//   { title: "NoSQL", key: "13" },
+//   { title: "MongoDB", key: "14" },
+//   { title: "Linux", key: "15" },
+//   { title: "AWS", key: "16" },
+//   { title: "Docker", key: "17" },
+//   { title: "Kubernetes", key: "18" },
+//   { title: "HTML", key: "19" },
+//   { title: "CSS", key: "20" },
+//   { title: "C", key: "21" },
+//   { title: "C++", key: "21" },
+// ];
+
 const skills = [
-  { title: "Java", key: "1" },
-  { title: "JavaScript", key: "2" },
-  { title: "React.js", key: "3" },
-  { title: "Angular.js", key: "4" },
-  { title: "Vue.js", key: "5" },
-  { title: "Python", key: "6" },
-  { title: "Go", key: "7" },
-  { title: "Node.js", key: "8" },
-  { title: "PHP", key: "9" },
-  { title: "JQuery", key: "10" },
-  { title: "Git", key: "11" },
-  { title: "SQL", key: "12" },
-  { title: "NoSQL", key: "13" },
-  { title: "MongoDB", key: "14" },
-  { title: "Linux", key: "15" },
-  { title: "AWS", key: "16" },
-  { title: "Docker", key: "17" },
-  { title: "Kubernetes", key: "18" },
-  { title: "HTML", key: "19" },
-  { title: "CSS", key: "20" },
-  { title: "C", key: "21" },
-  { title: "C++", key: "21" },
-];
+  'Java' , 
+  'Python' , 
+  'JavaScript', 
+  'CSS' , 'HTML' ,'Node.js' , 'React.js'
+]
 
 
--- src/components/Layout/NavBar/NavBar.js
+++ src/components/Layout/NavBar/NavBar.js
@@ -40,3 +40,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
