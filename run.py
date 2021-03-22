54e0d2d0e24288f0ca4d981ee2d7d3a81175da29
Ubuntu <ubuntu@ip-172-31-42-15.ap-south-1.compute.internal>
2021-03-22 18:52:29 +0000

    nginx conf and docker-compose updated

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 if __name__ == "__main__":
     # uvicorn.run("api:app", reload=True, debug=True)
-    uvicorn.run("api:app", relaod=True, host="0.0.0.0", debug=True)
+    uvicorn.run("api:app", reload=True, host="0.0.0.0", debug=True)
